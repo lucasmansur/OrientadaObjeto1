aef6b9b513c8f8a6d588d778699045ff7824f2a8
Lucas Mansur <38110444+srguello@users.noreply.github.com>
2018-08-28 22:00:09 -0300

    Add files via upload

---
+++ exercicio1/exe4.java
@@ -0,0 +1,17 @@
+/*Faça um programa que receba a quantidade de três produtos e o
+ * valor de três produtos, no seguinte formato: Quantidade1 Valor1 
+ * Quantidade2 Valor2 Quantidade3 Valor3. O programa deve calcular
+ * estes valores seguindo a fórmula total = Quantidade1 x Valor1 + 
+ * Quantidade2 x Valor2 + Quantidade3 x Valor3. O valor total deve 
+ * ser apresentado no final da execução do programa.
+
+*/
+
+class exe4 {
+public static void main (String[] args){
+   System.out.println ("Quantidades e valores\n") 
+    
+
+
+}
+}
